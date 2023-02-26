4630f6d029c7054551f5e716909c65b1abd78d7f
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-02-26 06:24:16 +0300

    Initial Commit

---
+++ encryption/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  encryption
+//
+//  Created by hema gomaa on 26/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
